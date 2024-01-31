17c507ca4e87741cc586a85f33221b96d73fce5e
Your Name <you@example.com>
2024-02-01 01:19:13 +0700

    import data consumer recived data from consumer

--- lib/providers/transaction_provider.dart
+++ lib/providers/transaction_provider.dart
@@ -2,21 +2,24 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter_application_1/models/transaction.dart';
 
-class TransactionProvider with ChangeNotifier {
+class TransactionProvider extends ChangeNotifier {
 //NOTE! notic when data has change!
-  List<Transaction> transaction = [
+  List<Transaction> transactions = [
     Transaction("guitar",5000,DateTime.now()),
     Transaction("shirt",500,DateTime.now()),
     Transaction("book",500,DateTime.now()),
+    Transaction("Medicine",390,DateTime.now()),
+    Transaction("Coffe",150,DateTime.now()),
+    Transaction("Meal",200,DateTime.now()),
   ];
 
   //NOTE! get data method
   List<Transaction> getTransaction(){
-    return transaction;
+    return transactions;
   }
 
   void addTransaction(Transaction statements){
-    transaction.add(statements);
+    transactions.add(statements);
   }
 
 }
